ba8af9f0b0f04c51775a5b9e774bf91db7f85de8
wooni97 <55667589+wooni97@users.noreply.github.com>
2022-12-02 00:14:35 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 51.82 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -7,28 +7,28 @@ def solution(queue1, queue2):
     max_count = len(q1) * 3
     if (s1 + s2) % 2 == 1 :
         return -1
-    
+
     while True :
         if s1 <= 0 or s2 <= 0 :
             answer = -1
             break
-            
+
         if s1 == same :
             break
-        
+
         if s1 > s2 :
             i = q1.popleft()
             q2.append(i)
-            s1 -= i 
+            s1 -= i
             s2 += i
             answer += 1
-        else : 
+        else :
             i = q2.popleft()
             q1.append(i)
-            s2 -= i 
+            s2 -= i
             s1 += i
             answer += 1
-        
+
         if answer == max_count:
             return -1
     return answer
