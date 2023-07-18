d1bcfcfe28f5a714dd882c35b211ebcff934797d
우니 <55667589+wooni97@users.noreply.github.com>
2023-07-18 09:11:42 +0900

    [Bronze II] Title: 블랙잭, Time: 144 ms, Memory: 42904 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,18 @@
+
+
+import sys
+from itertools import combinations
+
+n,m = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
+
+combi = list(combinations(arr,3))
+
+min = 1000000
+
+for c in combi :
+    if sum(c) <= m and m - sum(c) >= 0 and m - sum(c) < min:
+        min = m - sum(c)
+
+print(m - min)
+
