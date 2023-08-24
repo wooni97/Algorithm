e405db2990a4ab7a213e73a2983bb2e2bae2fd7b
우니 <55667589+wooni97@users.noreply.github.com>
2023-08-24 14:21:02 +0900

    [Silver IV] Title: 로프, Time: 3808 ms, Memory: 39268 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -1,18 +1,11 @@
+n = int(input())
+k = []
+for _ in range(n):
+    k.append(int(input()))
+k.sort()
 
-
-N = int(input())
-
-rope = []
-
-for i in range(N) :
-    rope.append(int(input()))
-
-rope.sort(reverse=True)
-
-answer = 0
-for i in range(N) :
-    if answer <= rope[i] * (i+1) :
-        answer = rope[i] * (i+1)
-   
-
-print(answer)
+answers = []
+for x in k:
+    answers.append(x*n)
+    n -= 1
+print(max(answers))
