75695020018794e9f6edc7310279aadbace0ad5f
우니 <55667589+wooni97@users.noreply.github.com>
2023-10-17 18:20:09 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 280 ms, Memory: 49216 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,43 +1,44 @@
 import sys
+
 input = lambda: sys.stdin.readline().rstrip()
 
-V, E = map(int, input().split())
+v, e = map(int, input().split())
 
-# Kruskal Algorithm
-# https://techblog-history-younghunjo1.tistory.com/262
+parent = [i for i in range(v + 1)]
+
 edges = []
-for _ in range(E):
-    A, B, C = map(int, input().split())
-    edges.append((A, B, C))
-edges.sort(key=lambda x: x[2]) # C(Cost)가 적은 것부터 정렬
+for _ in range(e):
+    node1, node2, weight = map(int, input().split())
+    edges.append((node1, node2, weight))
+
+edges.sort(key=lambda x: x[2])
 
 
-# Union-Find
-parent = [i for i in range(V+1)]
-
-def get_parent(x):
+def find(x):
     if parent[x] == x:
         return x
-    parent[x] = get_parent(parent[x]) # get_parent 거슬러 올라가면서 parent[x] 값도 갱신
+    parent[x] = find(parent[x])
     return parent[x]
 
-def union_parent(a, b):
-    a = get_parent(a)
-    b = get_parent(b)
 
-    if a < b: # 작은 쪽이 부모가 된다. (한 집합 관계라서 부모가 따로 있는 건 아님)
-        parent[b] = a
+def union(node1, node2):
+    node1 = find(node1)
+    node2 = find(node2)
+
+    if node1 < node2:
+        parent[node2] = node1
     else:
-        parent[a] = b        
-
-def same_parent(a, b):
-    return get_parent(a) == get_parent(b)
+        parent[node1] = node2
 
 
-answer = 0
-for a, b, cost in edges:
-    # cost가 작은 edge부터 하나씩 추가해가면서 같은 부모를 공유하지 않을 때(사이클 없을 때)만 확정
-    if not same_parent(a, b):
-        union_parent(a, b)
-        answer += cost
-print(answer)
+def same_parent(node1, node2):
+    return find(node1) == find(node2)
+
+
+min_weight = 0
+for node1, node2, weight in edges:
+    if not same_parent(node1, node2):
+        union(node1, node2)
+        min_weight += weight
+
+print(min_weight)
