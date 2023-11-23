ec2972851598ac3cfe94503b462e680d39e3f9b0
wooni97 <55667589+wooni97@users.noreply.github.com>
2023-11-23 23:08:08 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 70.91 ms, Memory: 17.7 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,38 +1,3 @@
-import sys
-
 def solution(gems):
-    
-    answer = [1, len(gems)]
-    
-    INF = sys.maxsize
-    MIN = INF
-    n = len(gems)
-    gems_set = set(gems)
-    end = 0 
-    
-    gem_dict = {}
-    
-    gem_dict[gems[0]] = 1 
-    
-    for start in range(n) :
-        while end < n and len(gem_dict) < len(gems_set) :
-            end += 1
-            
-            if end != n :
-                
-                if gems[end] not in gem_dict :
-                    gem_dict[gems[end]] = 1
-                else :
-                    gem_dict[gems[end]] += 1
-        
-        if end == n :
-            break
-        
-        if end - start < answer[1] - answer[0] :
-            answer = [start + 1, end + 1]
-            
-        gem_dict[gems[start]] -= 1
-        if gem_dict[gems[start]] == 0 :
-            del gem_dict[gems[start]]
-      
+    answer = []
     return answer
