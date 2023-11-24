1df742aea3d9f003101fb1f4d78f0eb29bc893a7
wooni97 <55667589+wooni97@users.noreply.github.com>
2023-11-24 10:35:31 +0900

    [level 4] Title: [카카오 인턴] 동굴 탐험, Time: 228.74 ms, Memory: 94.1 MB -BaekjoonHub

---
+++ 프로그래머스/4/67260. ［카카오 인턴］ 동굴 탐험/［카카오 인턴］ 동굴 탐험.py
@@ -0,0 +1,40 @@
+from collections import defaultdict, deque
+
+def solution(n, path, order):
+    
+    visited = [False] * n
+    graph = defaultdict(list)
+    
+    for node1, node2 in path :
+        graph[node1].append(node2)
+        graph[node2].append(node1)
+        
+    orders = [0 for _ in range(n)]
+    
+    for pre, post in order :
+        orders[post] = pre
+
+    
+    queue = deque()
+    queue.append(0)
+    
+    after = {}
+    
+    while queue :
+        visit_node = queue.popleft()
+        
+        if orders[visit_node] and visited[orders[visit_node]] == False :
+            after[orders[visit_node]] = visit_node
+            continue
+        
+        visited[visit_node] = True
+        
+        for next in graph[visit_node] :
+            if not visited[next] :
+                queue.append(next)
+        
+        if visit_node in after :
+            queue.append(after[visit_node])
+    
+    visit_count = visited.count(True)
+    return n == visit_count
