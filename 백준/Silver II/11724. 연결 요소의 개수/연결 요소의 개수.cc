ea3e8866162af7306cff58bea7aa5bc04990a511
wooni97 <jhw970714@gmail.com>
2024-02-15 13:06:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 296 ms, Memory: 6376 KB -BaekjoonHub

--- 백준/Silver II/11724. 연결 요소의 개수/연결 요소의 개수.cc
+++ 백준/Silver II/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -3,23 +3,22 @@
 #include <stack>
 
 using namespace std;
-
 void operator_dfs(int i, vector<bool>& visited, vector<vector<int>>& graph) {
     visited[i] = true;
-
+    
     for (int node : graph[i]) {
-        if (!visited[node]) {
+        if(!visited[node]) {
             operator_dfs(node, visited, graph);
         }
     }
 }
 
 int solution(int n, vector<vector<int>>& graph) {
-    vector<bool> visited(n + 1, false);
+    vector<bool> visited(n+1, false);
     int answer = 0;
 
-    for (int i = 1; i <= n; ++i) {
-        if (!visited[i]) {
+    for (int i = 1; i <= n; i++) {
+        if(!visited[i]) {
             operator_dfs(i, visited, graph);
             answer++;
         }
@@ -27,16 +26,16 @@ int solution(int n, vector<vector<int>>& graph) {
 
     return answer;
 }
-
 int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<int>> graph(n + 1);
+    vector<vector<int>> graph(n+1);
 
-    for (int i = 0; i < m; ++i) {
+    for (int i = 0; i < m; i++) {
         int node1, node2;
         cin >> node1 >> node2;
+
         graph[node1].push_back(node2);
         graph[node2].push_back(node1);
     }
