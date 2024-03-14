2fec29865aa1f4cfd7d1fc4e6306c32ab7d16672
정해운 <jhw970714@gmail.com>
2024-03-14 11:40:10 +0900

    [Gold V] Title: 계란으로 계란치기, Time: 4004 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
+++ 백준/Gold/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -1,44 +1,61 @@
-def BackTracking(start):
+import sys
 
-    global Answer
-    if start==N:
-        total=0
-        for i in range(N):
-            if egg[i][0]<=0:
-                total+=1
+input = sys.stdin.readline
+n = int(input())
+eggs = [list(map(int, input().split())) for _ in range(n)]
 
-        Answer=max(Answer,total)
+"""
+계란을 치는 과정
+1. 가장 왼쪽의 계란을 든다.
+2. 손에 들고 있는 계란으로 깨지지 않은 다른 계란 중에서 하나를 친다.
+   단, 손에 든 계란이 깨졌거나 깨지지 않은 다른 계란이 없으면 치지 않고 넘어간다.
+   이후 손에 든 계란을 원래 자리에 내려놓고 3번 과정을 진행한다.
+3. 가장 최근에 든 계란의 한 칸 오른쪽 계란을 손에 들고 2번 과정을 다시 진행한다.
+   단, 가장 최근에 든 계란이 가장 오른쪽에 위치한 계란일 경우 계란을 치는 과정을 종료한다.
+"""
+answer = 0
+
+
+def func(c):
+    global answer
+    if c == n:
+        count = 0
+        for egg in eggs:
+            if egg[0] <= 0:
+                count += 1
+        answer = max(answer, count)
         return
 
-    if egg[start][0]<=0:
-        BackTracking(start+1)
+    if eggs[c][0] <= 0:  # 계란이 깨진 상태라면
+        func(c + 1)
         return
 
-    check=True
-    for i in range(N):
-        if i==start:
+    check = True # 손에 든 계란 외 나머지 계란이 다 깨져있는지 확인하는 변수
+    for i in range(n):
+        if i == c:
             continue
-        if egg[i][0]>0:
-            check=False
-            break
 
-    if check: #다 깨져있는 경우
-        Answer=max(Answer , N-1) #자기빼고 전부다니깐 N-1
+        if eggs[i][0] > 0:
+            check = False
+            dur1, weight1 = eggs[c]
+            dur2, weight2 = eggs[i]
+
+            after_dur1 = dur1 - weight2
+            after_dur2 = dur2 - weight1
+
+            eggs[c][0] = after_dur1
+            eggs[i][0] = after_dur2
+
+            func(c + 1)
+
+            eggs[c][0] = dur1
+            eggs[i][0] = dur2
+
+    if check:
+        answer = max(answer, n - 1)
         return
-
-    for i in range(N):
-        if i==start or egg[i][0]<=0:
-            continue
-        egg[start][0]-=egg[i][1]
-        egg[i][0]-=egg[start][1]
-        BackTracking(start+1)
-        egg[start][0]+=egg[i][1]
-        egg[i][0]+=egg[start][1]
+    
 
 
-N=int(input())
-egg=[ list(map(int,input().split())) for _ in range(N) ] #내구도와 무게
-Answer=0
-BackTracking(0)
-
-print(Answer)
+func(0)
+print(answer)
