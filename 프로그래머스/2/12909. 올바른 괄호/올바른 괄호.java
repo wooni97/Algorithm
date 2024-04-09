11f1f682ce2772ef8cdeb3184647f6033a482641
정해운 <jhw970714@gmail.com>
2024-04-10 00:49:01 +0900

    [level 2] Title: 올바른 괄호, Time: 18.44 ms, Memory: 52.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else {
+                if (!stack.isEmpty()) stack.pop();
+                else return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
