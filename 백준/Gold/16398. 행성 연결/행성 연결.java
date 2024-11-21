a9905dd204dbd91c71b45b2e884ad90e96240118
정해운 <jhw970714@gmail.com>
2024-11-21 13:07:07 +0900

    [Gold IV] Title: 행성 연결, Time: 732 ms, Memory: 120484 KB -BaekjoonHub

---
+++ 백준/Gold/16398. 행성 연결/행성 연결.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                int cost = Integer.parseInt(st.nextToken());
+                if (i < j) {
+                    pq.add(new Edge(i, j, cost));
+                }
+            }
+        }
+
+        long result = 0;
+        int edgesUsed = 0;
+
+        while (edgesUsed < n - 1 && !pq.isEmpty()) {
+            Edge edge = pq.poll();
+            if (find(edge.u) != find(edge.v)) {
+                union(edge.u, edge.v);
+                result += edge.cost;
+                edgesUsed++;
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    private static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    private static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x != y) {
+            parent[y] = x;
+        }
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int u, v, cost;
+
+        public Edge(int u, int v, int cost) {
+            this.u = u;
+            this.v = v;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.cost, o.cost);
+        }
+    }
+}
